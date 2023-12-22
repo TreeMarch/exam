6791110560dba6626ac676d3945a34e2d5ce7947
TreeMarch <m5c0traan@gmail.com>
2023-12-22 21:10:14 +0700

    exam

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Student from './components/Student';
+
+function App() {
+  return (
+    <div className="App">
+      <Student/>
+    </div>
+  );
+}
+
+export default App;
